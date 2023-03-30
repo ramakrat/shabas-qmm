02bc184bcf3a366e9613bee9c1adaa1d9767eb11
Jeanie Lam <ljeaniez@gmail.com>
2023-03-30 00:17:06 -0400

    Added functional add/edit modals

--- src/components/Modals/AssessmentModal.tsx
+++ src/components/Modals/AssessmentModal.tsx
@@ -1,15 +1,75 @@
 import React from "react";
-import { Button, Card, CardActions, CardContent, CardHeader, IconButton, Modal, Paper, Step, StepButton, Stepper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Typography } from "@mui/material";
+import { Button, Card, CardActions, CardContent, CardHeader, FormControl, IconButton, InputLabel, MenuItem, Modal, Paper, Select, Step, StepButton, Stepper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Typography } from "@mui/material";
 import { Add, Close } from "@mui/icons-material";
+import { Assessment } from "@prisma/client";
+import { api } from "~/utils/api";
 
 interface Props {
     open: boolean;
     setOpen: any;
-    data?: any;
+    data?: Assessment;
 }
 
 const AssessmentModal: React.FC<Props> = (props) => {
+
     const { open, setOpen, data } = props;
+
+    // =========== Retrieve Form Context ===========
+
+    const sites = api.site.getAll.useQuery().data;
+    const engagements = api.engagement.getAll.useQuery().data;
+
+    // =========== Input Field States ===========
+
+    const [status, setStatus] = React.useState<string>('');
+    const [startDate, setStartDate] = React.useState<Date>(new Date());
+    const [endDate, setEndDate] = React.useState<Date>(new Date());
+    const [description, setDescription] = React.useState<string>('');
+    const [siteId, setSiteId] = React.useState<number>(1);
+    const [engagementId, setEngagementId] = React.useState<number>(1);
+
+    // =========== Submission Management ===========
+
+    const create = api.assessment.create.useMutation();
+    const update = api.assessment.update.useMutation();
+
+    React.useEffect(() => {
+        if (data) {
+            setStatus(data.status);
+            setStartDate(data.start_date);
+            setEndDate(data.end_date);
+            setDescription(data.description);
+            setSiteId(data.site_id);
+            setEngagementId(data.engagement_id);
+        }
+    }, [data])
+
+    const handleSubmit = (event: React.SyntheticEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (data) {
+            update.mutate({
+                id: data.id,
+                status: status,
+                start_date: startDate,
+                end_date: endDate,
+                description: description,
+                site_id: siteId,
+                engagement_id: engagementId,
+            })
+        } else {
+            create.mutate({
+                status: status,
+                start_date: startDate,
+                end_date: endDate,
+                description: description,
+                site_id: siteId,
+                engagement_id: engagementId,
+            })
+        }
+        if (create.isSuccess || update.isSuccess)
+            setOpen(false)
+    }
+
 
     const [question, setQuestion] = React.useState<number>(1);
 
@@ -43,92 +103,144 @@ const AssessmentModal: React.FC<Props> = (props) => {
 
     return (
         <Modal open={open} onClose={() => setOpen(false)} className='create-modal lg'>
-            <Card>
-                <CardHeader
-                    title={data ? 'Edit Assessment' : 'Create New Assessment'}
-                    action={
-                        <IconButton onClick={() => setOpen(false)}>
-                            <Close />
-                        </IconButton>
-                    }
-                />
-                <CardContent>
-                    <Stepper nonLinear activeStep={activeStep}>
-                        {steps.map((label, index) => (
-                            <Step key={label} completed={completed[index]}>
-                                <StepButton color="inherit" onClick={handleStep(index)}>
-                                    {label}
-                                </StepButton>
-                            </Step>
-                        ))}
-                    </Stepper>
-                    {activeStep == 0 &&
-                        <div className='form-info'>
-                            <TextField id='title' name='title' label='title' size='small' />
-                            <TextField id='title' name='title' label='title' size='small' />
-                        </div>
-                    }
-                    {activeStep == 1 &&
-                        <div className='form-questions'>
-                            <TableContainer component={Paper}>
-                                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                                    <TableHead>
-                                        <TableRow>
-                                            <TableCell align="center">Question #</TableCell>
-                                            <TableCell align="center">Industry</TableCell>
-                                            <TableCell align="center">API Segment</TableCell>
-                                            <TableCell align="center">Site Specific</TableCell>
-                                            <TableCell align="left">Content</TableCell>
-                                        </TableRow>
-                                    </TableHead>
-                                    <TableBody>
-                                        {rows.map((row) => (
-                                            <TableRow
-                                                key={row.name}
-                                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                            >
-                                                <TableCell align="center" component="th" scope="row">
-                                                    {row.name}
-                                                </TableCell>
-                                                <TableCell align="center">
-                                                    {row.industry}
-                                                </TableCell>
-                                                <TableCell align="center">
-                                                    {row.api}
-                                                </TableCell>
-                                                <TableCell align="center">
-                                                    {row.site}
-                                                </TableCell>
-                                                <TableCell align="left">{row.content}</TableCell>
+            <form onSubmit={handleSubmit}>
+                <Card>
+                    <CardHeader
+                        title={data ? 'Edit Assessment' : 'Create New Assessment'}
+                        action={
+                            <IconButton onClick={() => setOpen(false)}>
+                                <Close />
+                            </IconButton>
+                        }
+                    />
+                    <CardContent>
+                        <Stepper nonLinear activeStep={activeStep}>
+                            {steps.map((label, index) => (
+                                <Step key={label} completed={completed[index]}>
+                                    <StepButton color="inherit" onClick={handleStep(index)}>
+                                        {label}
+                                    </StepButton>
+                                </Step>
+                            ))}
+                        </Stepper>
+                        {activeStep == 0 &&
+                            <div className='form-info'>
+                                <FormControl>
+                                    <InputLabel size="small">Site</InputLabel>
+                                    <Select
+                                        name='siteId' label='Site' size='small'
+                                        value={siteId}
+                                        onChange={e => setSiteId(Number(e.target.value))}
+                                    >
+                                        {sites && sites.map(o => {
+                                            return (
+                                                <MenuItem value={o.id} key={o.id}>
+                                                    {o.id} {o.name}
+                                                </MenuItem>
+                                            )
+                                        })}
+                                    </Select>
+                                </FormControl>
+                                <FormControl>
+                                    <InputLabel size="small">Engagement</InputLabel>
+                                    <Select
+                                        name='engagementId' label='Engagement' size='small'
+                                        value={siteId}
+                                        onChange={e => setEngagementId(Number(e.target.value))}
+                                    >
+                                        {engagements && engagements.map(o => {
+                                            return (
+                                                <MenuItem value={o.id} key={o.id}>
+                                                    {o.id} {o.description}
+                                                </MenuItem>
+                                            )
+                                        })}
+                                    </Select>
+                                </FormControl>
+                                <TextField
+                                    name='status' label='Status' size='small'
+                                    value={status}
+                                    onChange={e => setStatus(e.target.value)}
+                                />
+                                <TextField
+                                    name='startDate' label='Start Date' size='small'
+                                    value={startDate}
+                                    onChange={e => setStartDate(new Date(e.target.value))}
+                                />
+                                <TextField
+                                    name='endDate' label='End Date' size='small'
+                                    value={endDate}
+                                    onChange={e => setEndDate(new Date(e.target.value))}
+                                />
+                                <TextField
+                                    name='description' label='Description' size='small'
+                                    value={description}
+                                    onChange={e => setDescription(e.target.value)}
+                                />
+                            </div>
+                        }
+                        {activeStep == 1 &&
+                            <div className='form-questions'>
+                                <TableContainer component={Paper}>
+                                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                                        <TableHead>
+                                            <TableRow>
+                                                <TableCell align="center">Question #</TableCell>
+                                                <TableCell align="center">Industry</TableCell>
+                                                <TableCell align="center">API Segment</TableCell>
+                                                <TableCell align="center">Site Specific</TableCell>
+                                                <TableCell align="left">Content</TableCell>
                                             </TableRow>
-                                        ))}
-                                    </TableBody>
-                                </Table>
-                            </TableContainer>
-                            {addQuestion ?
-                                <div className='questions-bank'>
-                                    <Typography className={question == 1 ? 'active' : ''} onClick={() => setQuestion(1)}>S1</Typography>
-                                    <Typography className={question == 2 ? 'active' : ''} onClick={() => setQuestion(2)}>S2</Typography>
-                                    <Typography className={question == 3 ? 'active' : ''} onClick={() => setQuestion(3)}>S3</Typography>
-                                    <Typography className={question == 4 ? 'active' : ''} onClick={() => setQuestion(4)}>S4</Typography>
-                                    <Typography className={question == 5 ? 'active' : ''} onClick={() => setQuestion(5)}>S5</Typography>
-                                    <Typography className={question == 6 ? 'active' : ''} onClick={() => setQuestion(6)}>S6</Typography>
-                                    <Typography className={question == 7 ? 'active' : ''} onClick={() => setQuestion(7)}>S7</Typography>
-                                    <Button variant='contained' onClick={() => { setAddQuestion(false) }}><Add />Add Question to Assessment</Button>
-                                </div> :
-                                <Button variant='contained' onClick={() => { setAddQuestion(true) }}><Add />Add Question</Button>
-                            }
-                        </div>
-                    }
-                </CardContent>
-                <CardActions>
-                    <Button variant='contained' color='error' onClick={() => setOpen(false)}>Cancel</Button>
-                    {data ?
-                        <Button variant='contained'>Save</Button> :
-                        <Button variant='contained'>Create</Button>
-                    }
-                </CardActions>
-            </Card>
+                                        </TableHead>
+                                        <TableBody>
+                                            {rows.map((row) => (
+                                                <TableRow
+                                                    key={row.name}
+                                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                                >
+                                                    <TableCell align="center" component="th" scope="row">
+                                                        {row.name}
+                                                    </TableCell>
+                                                    <TableCell align="center">
+                                                        {row.industry}
+                                                    </TableCell>
+                                                    <TableCell align="center">
+                                                        {row.api}
+                                                    </TableCell>
+                                                    <TableCell align="center">
+                                                        {row.site}
+                                                    </TableCell>
+                                                    <TableCell align="left">{row.content}</TableCell>
+                                                </TableRow>
+                                            ))}
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
+                                {addQuestion ?
+                                    <div className='questions-bank'>
+                                        <Typography className={question == 1 ? 'active' : ''} onClick={() => setQuestion(1)}>S1</Typography>
+                                        <Typography className={question == 2 ? 'active' : ''} onClick={() => setQuestion(2)}>S2</Typography>
+                                        <Typography className={question == 3 ? 'active' : ''} onClick={() => setQuestion(3)}>S3</Typography>
+                                        <Typography className={question == 4 ? 'active' : ''} onClick={() => setQuestion(4)}>S4</Typography>
+                                        <Typography className={question == 5 ? 'active' : ''} onClick={() => setQuestion(5)}>S5</Typography>
+                                        <Typography className={question == 6 ? 'active' : ''} onClick={() => setQuestion(6)}>S6</Typography>
+                                        <Typography className={question == 7 ? 'active' : ''} onClick={() => setQuestion(7)}>S7</Typography>
+                                        <Button variant='contained' onClick={() => { setAddQuestion(false) }}><Add />Add Question to Assessment</Button>
+                                    </div> :
+                                    <Button variant='contained' onClick={() => { setAddQuestion(true) }}><Add />Add Question</Button>
+                                }
+                            </div>
+                        }
+                    </CardContent>
+                    <CardActions>
+                        <Button variant='contained' color='error' onClick={() => setOpen(false)}>Cancel</Button>
+                        {data ?
+                            <Button variant='contained' type='submit'>Save</Button> :
+                            <Button variant='contained' type='submit'>Create</Button>
+                        }
+                    </CardActions>
+                </Card>
+            </form>
         </Modal>
     )
 }
--- src/components/Modals/ClientModal.tsx
+++ src/components/Modals/ClientModal.tsx
@@ -1,56 +1,161 @@
+import React from "react";
 import { Button, Card, CardActions, CardContent, CardHeader, FormControl, IconButton, InputLabel, MenuItem, Modal, Select, TextField } from "@mui/material";
 import { Close } from "@mui/icons-material";
 import { Countries } from "~/utils/utils";
+import { api } from "~/utils/api";
+import { Client } from "@prisma/client";
 
 interface Props {
     open: boolean;
     setOpen: any;
-    data?: any;
+    data?: Client;
 }
 
 const ClientModal: React.FC<Props> = (props) => {
+
     const { open, setOpen, data } = props;
+
+    // =========== Input Field States ===========
+
+    const [firstName, setFirstName] = React.useState<string>('');
+    const [lastName, setLastName] = React.useState<string>('');
+    const [streetAddress, setStreetAddress] = React.useState<string>('');
+    const [city, setCity] = React.useState<string>('');
+    const [state, setState] = React.useState<string>('');
+    const [zipCode, setZipCode] = React.useState<string>('');
+    const [country, setCountry] = React.useState<string>('US');
+    const [description, setDescription] = React.useState<string>('');
+
+
+    // =========== Submission Management ===========
+
+    const create = api.client.create.useMutation();
+    const update = api.client.update.useMutation();
+
+    React.useEffect(() => {
+        if (data) {
+            setFirstName(data.first_name);
+            setLastName(data.last_name);
+            setStreetAddress(data.street_address);
+            setCity(data.city);
+            setState(data.state);
+            setZipCode(data.zip_code);
+            setCountry(data.country);
+            setDescription(data.description);
+        }
+    }, [data])
+
+    // React.useEffect(() => {
+    //     if (create.isSuccess == true || update.isSuccess == true)
+    //         setOpen(false)
+    // }, [create, update])
+
+    const handleSubmit = (event: React.SyntheticEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (data) {
+            update.mutate({
+                id: data.id,
+                first_name: firstName,
+                last_name: lastName,
+                street_address: streetAddress,
+                city: city,
+                state: state,
+                country: country,
+                zip_code: zipCode,
+                description: description,
+            })
+        } else {
+            create.mutate({
+                first_name: firstName,
+                last_name: lastName,
+                street_address: streetAddress,
+                city: city,
+                state: state,
+                country: country,
+                zip_code: zipCode,
+                description: description,
+            })
+        }
+        if (create.isSuccess || update.isSuccess)
+            setOpen(false)
+    }
+
 
     return (
         <Modal open={open} onClose={() => setOpen(false)} className='create-modal'>
-            <Card>
-                <CardHeader
-                    title={data ? 'Edit Client' : 'Create New Client'}
-                    action={
-                        <IconButton onClick={() => setOpen(false)}>
-                            <Close />
-                        </IconButton>
-                    }
-                />
-                <CardContent>
-                    <TextField name='firstName' label='First Name' size='small' />
-                    <TextField name='lastName' label='Last Name' size='small' />
-                    <TextField name='streetAddress' label='Street Address' size='small' />
-                    <TextField name='city' label='City' size='small' />
-                    <TextField name='state' label='State' size='small' />
-                    <TextField name='zipCode' label='Zip Code' size='small' />
-                    <FormControl>
-                        <InputLabel size="small">Country</InputLabel>
-                        <Select name='country' size='small'>
-                            {Countries.map((country) => {
-                                return (
-                                    <MenuItem value={country.code}>
-                                        {country.name}
-                                    </MenuItem>
-                                )
-                            })}
-                        </Select>
-                    </FormControl>
-                    <TextField name='description' label='Description' size='small' multiline minRows={3} />
-                </CardContent>
-                <CardActions>
-                    <Button variant='contained' color='error' onClick={() => setOpen(false)}>Cancel</Button>
-                    {data ?
-                        <Button variant='contained'>Save</Button> :
-                        <Button variant='contained'>Create</Button>
-                    }
-                </CardActions>
-            </Card>
+            <form onSubmit={handleSubmit}>
+                <Card>
+                    <CardHeader
+                        title={data ? 'Edit Client' : 'Create New Client'}
+                        action={
+                            <IconButton onClick={() => setOpen(false)}>
+                                <Close />
+                            </IconButton>
+                        }
+                    />
+                    <CardContent>
+                        <TextField
+                            name='firstName' label='First Name' size='small'
+                            value={firstName}
+                            onChange={e => setFirstName(e.target.value)}
+                        />
+                        <TextField
+                            name='lastName' label='Last Name' size='small'
+                            value={lastName}
+                            onChange={e => setLastName(e.target.value)}
+                        />
+                        <TextField
+                            name='streetAddress' label='Street Address' size='small'
+                            value={streetAddress}
+                            onChange={e => setStreetAddress(e.target.value)}
+                        />
+                        <TextField
+                            name='city' label='City' size='small'
+                            value={city}
+                            onChange={e => setCity(e.target.value)}
+                        />
+                        <TextField
+                            name='state' label='State' size='small'
+                            value={state}
+                            onChange={e => setState(e.target.value)}
+                        />
+                        <TextField
+                            name='zipCode' label='Zip Code' size='small'
+                            value={zipCode}
+                            onChange={e => setZipCode(e.target.value)}
+                        />
+                        <FormControl>
+                            <InputLabel size="small">Country</InputLabel>
+                            <Select
+                                name='country' size='small' label='Country'
+                                value={country}
+                                onChange={e => setCountry(e.target.value)}
+                            >
+                                {Countries.map((country) => {
+                                    return (
+                                        <MenuItem value={country.code} key={country.code}>
+                                            {country.name}
+                                        </MenuItem>
+                                    )
+                                })}
+                            </Select>
+                        </FormControl>
+                        <TextField
+                            name='description' label='Description' size='small'
+                            multiline minRows={3}
+                            value={description}
+                            onChange={e => setDescription(e.target.value)}
+                        />
+                    </CardContent>
+                    <CardActions>
+                        <Button variant='contained' color='error' onClick={() => setOpen(false)}>Cancel</Button>
+                        {data ?
+                            <Button variant='contained' type='submit'>Save</Button> :
+                            <Button variant='contained' type='submit'>Create</Button>
+                        }
+                    </CardActions>
+                </Card>
+            </form>
         </Modal>
     )
 }
