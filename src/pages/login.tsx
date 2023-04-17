4421cc709c8b6a13a61e6029f7bfb9501313d9d8
Jeanie Lam <ljeaniez@gmail.com>
2023-04-16 23:25:31 -0400

    Added foundational login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,56 @@
 import type { NextPage } from "next";
 
-const Login: NextPage = () => {
-    return (
-        <div>
+import * as yup from "yup";
+import { Field, Form, Formik, type FormikHelpers, type FormikProps } from "formik";
 
+import { Button, Card, Typography } from "@mui/material";
+import TextField from "~/components/Form/TextField";
+
+interface FormValues {
+    username: string;
+    password: string;
+}
+
+const validationSchema = yup.object().shape({
+    username: yup.string().required("Required"),
+    password: yup.string().required("Required"),
+});
+
+const Login: NextPage = () => {
+
+    const handleSubmit = () => {
+        return;
+    }
+
+    return (
+        <div className='login'>
+            <Card>
+                <Formik
+                    enableReinitialize
+                    initialValues={{
+                        username: '',
+                        password: '',
+                    }}
+                    validationSchema={validationSchema}
+                    validateOnBlur={false}
+                    validateOnChange={false}
+                    onSubmit={handleSubmit}
+                >
+                    <Form>
+                        <Typography variant='h2'>Shabas Logo</Typography>
+                        <Typography variant='h2'>Login</Typography>
+                        <Field
+                            name='username' label='Username' size='small'
+                            component={TextField}
+                        />
+                        <Field
+                            name='password' label='Password' size='small'
+                            component={TextField}
+                        />
+                        <Button variant='contained'>Login</Button>
+                    </Form>
+                </Formik>
+            </Card>
         </div>
     );
 };
