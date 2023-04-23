b3312c52c6f43f038a8313db6310c63ddddcd984
Jeanie Lam <ljeaniez@gmail.com>
2023-04-23 17:29:41 -0400

    Created question table and removed sidebar

--- src/pages/questions/index.tsx
+++ src/pages/questions/index.tsx
@@ -1,43 +1,97 @@
 import * as React from 'react';
 import { type NextPage } from "next";
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
-import { Grid } from '@mui/material';
+import { Button, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 
 import { api } from "~/utils/api";
 import Layout from "~/components/Layout/Layout";
-import QuestionsSidebar from '~/components/Assessment/QuestionsSidebar';
+import { Add, Edit } from '@mui/icons-material';
+import QuestionModal from '~/components/Modals/QuestionModal';
 
 const Question: NextPage = () => {
 
     const { push } = useRouter();
+    const [questionModal, setQuestionModal] = React.useState<boolean>(false);
 
-    const setQuestionSelection = (question: number) => {
-        // eslint-disable-next-line @typescript-eslint/no-floating-promises
-        push(`/questions/${question}`);
-    }
+    // const setQuestionSelection = (question: number) => {
+    //     // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    //     push(`/questions/${question}`);
+    // }
 
     // =========== Retrieve Form Context ===========
 
     const questions = api.question.getAll.useQuery(true).data;
 
     return (
-        <Layout active='questions'>
-            <div className='assessment'>
-                <Grid container spacing={2}>
-                    <Grid item xs={2}>
-                        {questions &&
-                            <QuestionsSidebar
-                                questions={questions}
-                                question={-1}
-                                setQuestion={setQuestionSelection}
-                                addOption
-                            />
-                        }
-                    </Grid>
-                </Grid>
-            </div>
-        </Layout>
+        <>
+            <Layout active='questions'>
+                <div className='dashboard'>
+                    <div className='browse-add'>
+                        <Button
+                            variant='contained'
+                            endIcon={<Add />}
+                            onClick={() => { setQuestionModal(true) }}
+                        >
+                            New Question
+                        </Button>
+                    </div>
+                    <TableContainer component={Paper}>
+                        <Table sx={{ minWidth: 650 }} size="small">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell align="center">Number</TableCell>
+                                    <TableCell align="left">Question</TableCell>
+                                    <TableCell align="left">Pillar</TableCell>
+                                    <TableCell align="left">Practice Area</TableCell>
+                                    <TableCell align="left">Topic Area</TableCell>
+                                    <TableCell align="left">Active</TableCell>
+                                    <TableCell align="center">Edit</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {questions && questions.map((data, i) => {
+                                    return (
+                                        <TableRow
+                                            key={i}
+                                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                        >
+                                            <TableCell align="center">
+                                                {data.number}
+                                            </TableCell>
+                                            <TableCell align="left">
+                                                {data.question}
+                                            </TableCell>
+                                            <TableCell align="left">
+                                                {data.pillar}
+                                            </TableCell>
+                                            <TableCell align="left">
+                                                {data.practice_area}
+                                            </TableCell>
+                                            <TableCell align="left">
+                                                {data.topic_area}
+                                            </TableCell>
+                                            <TableCell align="left">
+                                                {data.active ? 'True' : 'False'}
+                                            </TableCell>
+                                            <TableCell align="center">
+                                                <Link href={`/questions/${data.id}`}>
+                                                    <IconButton>
+                                                        <Edit fontSize='small' />
+                                                    </IconButton>
+                                                </Link>
+                                            </TableCell>
+                                        </TableRow>
+                                    )
+                                })}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </div>
+            </Layout>
+            <QuestionModal open={questionModal} setOpen={setQuestionModal} />
+        </>
     );
 };
 
