1b6991e20f1132e51ae90b70bf2aaa9789ce02ab
Jeanie Lam <ljeaniez@gmail.com>
2023-05-01 02:25:09 -0400

    Replace root with login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,113 +1,67 @@
-import * as React from 'react';
-import { type NextPage } from "next";
+import type { NextPage } from "next";
+import Image from 'next/image';
 
-import { Box, Tab, Tabs } from '@mui/material';
+import * as yup from "yup";
+import { Field, Form, Formik, type FormikHelpers } from "formik";
 
-import { api } from '~/utils/api';
-import Layout from "~/components/Layout/Layout";
-import BrowseClients from '~/components/Administrator/Clients/BrowseClients';
-import BrowseAssessments from '~/components/Administrator/Assessments/BrowseAssessments';
-import BrowsePocs from '~/components/Administrator/Pocs/BrowsePocs';
+import { Button, Card, Typography } from "@mui/material";
+import TextField from "~/components/Form/TextField";
 
-const AdminDashboard: NextPage = () => {
+import logo from '../components/Layout/logo.png';
 
-    const [value, setValue] = React.useState(0);
 
-    interface TabPanelProps {
-        children?: React.ReactNode;
-        index: number;
-        value: number;
+interface FormValues {
+    username: string;
+    password: string;
+}
+
+const validationSchema = yup.object().shape({
+    username: yup.string().required("Required"),
+    password: yup.string().required("Required"),
+});
+
+const Login: NextPage = () => {
+
+    const handleSubmit = (
+        values: FormValues,
+        formikHelpers: FormikHelpers<FormValues>
+    ) => {
+        return;
     }
-
-    const TabPanel = (props: TabPanelProps) => {
-        const { children, value, index, ...other } = props;
-
-        return (
-            <div
-                role="tabpanel"
-                hidden={value !== index}
-                id={`simple-tabpanel-${index}`}
-                aria-labelledby={`simple-tab-${index}`}
-                className='tab-panel'
-                {...other}
-            >
-                {value === index && (
-                    <Box sx={{ p: 3 }}>
-                        {children}
-                    </Box>
-                )}
-            </div>
-        );
-    }
-
-    // =========== Add Object Modal State Management ===========
-    // Keep modal boolean states here to trigger count query update
-
-    const [clientModal, setClientModal] = React.useState<boolean>(false);
-    const [siteModal, setSiteModal] = React.useState<boolean>(false);
-    const [engagementModal, setEngagementModal] = React.useState<boolean>(false);
-    const [assessmentModal, setAssessmentModal] = React.useState<boolean>(false);
-    const [pocModal, setPOCModal] = React.useState<boolean>(false);
-
-
-
-    // TODO: Make dynamic
-    const totalClient = api.client.getTotalCount.useQuery().data;
-    const totalSite = api.site.getTotalCount.useQuery(siteModal).data;
-    const totalEngagement = api.engagement.getTotalCount.useQuery(engagementModal).data;
-    const totalAssessment = api.assessment.getTotalCount.useQuery(assessmentModal).data;
-    const totalPOC = api.poc.getTotalCount.useQuery(pocModal).data;
 
     return (
-        <Layout active='dashboard'>
-            <div className='dashboard'>
-                <Box sx={{ width: '100%' }}>
-                    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                        <Tabs
-                            value={value}
-                            onChange={(_event: React.SyntheticEvent, newValue: number) => setValue(newValue)}
-                        >
-                            <Tab label={
-                                <div className='filter'>
-                                    <span className='label'>Clients</span>
-                                    <span className='count'>{totalClient}</span>
-                                </div>
-                            } />
-                            <Tab label={
-                                <div className='filter'>
-                                    <span className='label'>Sites</span>
-                                    <span className='count'>{totalSite}</span>
-                                </div>
-                            } />
-                            <Tab label={
-                                <div className='filter'>
-                                    <span className='label'>POC</span>
-                                    <span className='count'>{totalPOC}</span>
-                                </div>
-                            } />
-                            <Tab label={<>
-                                <div className='filter'>
-                                    <span className='label'>Engagement</span>
-                                    <span className='count'>{totalEngagement}</span>
-                                    <span className='label'> / Assessments</span>
-                                    <span className='count'>{totalAssessment}</span>
-                                </div>
-                            </>} />
-                        </Tabs>
-                    </Box>
-                    <TabPanel value={value} index={0}>
-                        <BrowseClients clientModal={clientModal} setClientModal={setClientModal} siteModal={siteModal} setSiteModal={setSiteModal} />
-                    </TabPanel>
-                    <TabPanel value={value} index={2}>
-                        <BrowsePocs pocModal={pocModal} setPOCModal={setPOCModal} />
-                    </TabPanel>
-                    <TabPanel value={value} index={3}>
-                        <BrowseAssessments engagementModal={engagementModal} setEngagementModal={setEngagementModal} assessmentModal={assessmentModal} setAssessmentModal={setAssessmentModal} />
-                    </TabPanel>
-                </Box>
-            </div>
-        </Layout>
+        <div className='login'>
+            <Card>
+                <Formik
+                    enableReinitialize
+                    initialValues={{
+                        username: '',
+                        password: '',
+                    }}
+                    validationSchema={validationSchema}
+                    validateOnBlur={false}
+                    validateOnChange={false}
+                    onSubmit={handleSubmit}
+                >
+                    <Form>
+                        <div className='logo'>
+                            <Image src={logo} alt={'Shabas Logo'} height={100} />
+                        </div>
+                        <Typography variant='h2'>Login</Typography>
+                        <Field
+                            name='username' label='Username' size='small'
+                            component={TextField}
+                        />
+                        <Field
+                            name='password' label='Password' size='small'
+                            component={TextField}
+                        />
+                        <Button variant='contained'>Login</Button>
+                    </Form>
+                </Formik>
+            </Card>
+        </div>
     );
 };
 
-export default AdminDashboard;
+export default Login;
