7ad093f90b20206a112301553af98fc662036114
Jeanie Lam <ljeaniez@gmail.com>
2023-06-27 14:00:09 -0400

    Temporary homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { NextPage } from "next";
 import { useSession } from "next-auth/react";
 import Layout from "~/components/Layout/Layout";
+import { Card } from "@mui/material";
 
 const Homepage: NextPage = () => {
 
@@ -10,9 +11,14 @@ const Homepage: NextPage = () => {
     return (
         <Layout session={session} requiredRoles={['ADMIN', 'ASSESSOR', 'LEAD_ASSESSOR', 'OVERSIGHT_ASSESSOR']}>
             <div className='page-message'>
-                <span className='title'>
-                    Welcome to Shabas QMM
-                </span>
+                <Card>
+                    <span className='title'>
+                        Welcome to Shabas QMM
+                    </span>
+                    <span className='subtitle'>
+                        Select a feature at the top to get started
+                    </span>
+                </Card>
             </div>
         </Layout>
     );
