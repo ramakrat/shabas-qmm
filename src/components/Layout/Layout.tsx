fcb7b40c5852dbf1735600f2b1c111e8c48361ca
Jeanie Lam <ljeaniez@gmail.com>
2023-06-27 13:49:01 -0400

    Update nav bar to contain user's name and role

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,13 +2,14 @@ import * as React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
-import { IconButton, Menu, MenuItem, Switch } from '@mui/material';
-import { Settings } from '@mui/icons-material';
+import { Button, IconButton, Menu, MenuItem, Switch } from '@mui/material';
+import { ArrowDownward, KeyboardArrowDown, Settings } from '@mui/icons-material';
 import logo from './logo.png';
 import { api } from '~/utils/api';
 import { useRouter } from 'next/router';
 import { signOut } from 'next-auth/react';
 import { Session } from 'next-auth';
+import { underscoreToTitle } from '~/utils/utils';
 
 interface Props {
     session: Session | null;
@@ -38,7 +39,6 @@ export const Layout: React.FC<Props> = (props) => {
     }
 
     const permitted = session?.user.role && requiredRoles?.includes(session.user.role) && !accessDenied;
-    const [adminRole, setAdminRole] = React.useState<boolean>(session?.user.role == 'ADMIN');
 
     // TODO: Make dynamic
     const totalClient = api.client.getTotalCount.useQuery().data;
@@ -47,6 +47,103 @@ export const Layout: React.FC<Props> = (props) => {
     const totalAssessment = api.assessment.getTotalCount.useQuery(true).data;
     const totalPOC = api.poc.getTotalCount.useQuery(true).data;
     const totalQuestion = api.question.getTotalCount.useQuery(true).data;
+
+
+    const getRoleNavItems = (role: string) => {
+        if (role == 'ADMIN') {
+            return (
+                <>
+                    <Link href={'/clients'} className={'nav-item ' + (active == 'clients' ? 'active' : '')}>
+                        <span className='label'>Clients</span>
+                        <span className='count'>{totalClient ?? 0}</span>
+                        <span className='label child-label'>/ Sites</span>
+                        <span className='count'>{totalSite ?? 0}</span>
+                    </Link>
+                    <Link href={'/pocs'} className={'nav-item ' + (active == 'pocs' ? 'active' : '')}>
+                        <span className='label'>POC</span>
+                        <span className='count'>{totalPOC ?? 0}</span>
+                    </Link>
+                    <Link href={'/assessments'} className={'nav-item ' + (active == 'assessments' ? 'active' : '')}>
+                        <span className='label'>Engagements</span>
+                        <span className='count'>{totalEngagement ?? 0}</span>
+                        <span className='label child-label'>/ Assessments</span>
+                        <span className='count'>{totalAssessment ?? 0}</span>
+                    </Link>
+                    <Link href={'/questions'} className={'nav-item ' + (active == 'questions' ? 'active' : '')}>
+                        <span className='label'>Question</span>
+                        <span className='count'>{totalQuestion ?? 0}</span>
+                    </Link>
+                    <Link href={'/completed-assessments'} className={'nav-item ' + (active == 'completed-assessments' ? 'active' : '')}>
+                        <span className='label'>
+                            Completed Assessments
+                        </span>
+                    </Link>
+                </>
+            )
+        }
+        if (role == 'ASSESSOR') {
+            return (
+                <Link href={'/ongoing-assessments'} className={'nav-item ' + (active == 'ongoing-assessments' ? 'active' : '')}>
+                    <span className='label'>
+                        Ongoing Assessments
+                    </span>
+                </Link>
+            )
+        }
+        if (role == 'LEAD_ASSESSOR') {
+            return (
+                <>
+                    <Link href={'/ongoing-assessments'} className={'nav-item ' + (active == 'ongoing-assessments' ? 'active' : '')}>
+                        <span className='label'>
+                            Ongoing Assessments
+                        </span>
+                    </Link>
+                    <Link href={'/review-assessments'} className={'nav-item ' + (active == 'review-assessments' ? 'active' : '')}>
+                        <span className='label'>
+                            Review Assessments
+                        </span>
+                    </Link>
+                </>
+            )
+        }
+        if (role == 'OVERSIGHT_ASSESSOR') {
+            return (
+                <Link href={'/oversight-assessments'} className={'nav-item ' + (active == 'oversight-assessments' ? 'active' : '')}>
+                    <span className='label'>
+                        Oversight Assessments
+                    </span>
+                </Link>
+            )
+        }
+        return undefined;
+    }
+
+    const userFeatures = (
+        <>
+            {session?.user.name ?
+                <Button onClick={handleClick} className='user-button'>
+                    <div className='user-name'>
+                        <span>{session.user.name}</span>
+                        <span>{underscoreToTitle(session.user.role)}</span>
+                    </div>
+                    <KeyboardArrowDown />
+                </Button> :
+                <IconButton onClick={handleClick}>
+                    <Settings />
+                </IconButton>
+            }
+            <Menu
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+            >
+                <MenuItem onClick={handleClose}>Reset Password</MenuItem>
+                {session?.user.role == 'ADMIN' && <MenuItem onClick={() => { router.push('/management'); handleClose(); }}>User Management</MenuItem>}
+                <MenuItem onClick={handleLogout}>Logout</MenuItem>
+            </Menu>
+        </>
+
+    )
 
     if (!permitted) {
         return (
@@ -63,18 +160,7 @@ export const Layout: React.FC<Props> = (props) => {
                                 <Image src={logo} alt={'Shabas Logo'} height={45} />
                             </Link>
                         </div>
-                        <IconButton onClick={handleClick}>
-                            <Settings />
-                        </IconButton>
-                        <Menu
-                            anchorEl={anchorEl}
-                            open={open}
-                            onClose={handleClose}
-                        >
-                            <MenuItem onClick={handleClose}>Profile</MenuItem>
-                            <MenuItem onClick={() => { router.push('/management'); handleClose(); }}>User Management</MenuItem>
-                            <MenuItem onClick={handleLogout}>Logout</MenuItem>
-                        </Menu>
+                        {userFeatures}
                     </div>
                     <div className='page-message'>
                         <span className='title'>
@@ -101,66 +187,9 @@ export const Layout: React.FC<Props> = (props) => {
                         <Link href='/' className='logo'>
                             <Image src={logo} alt={'Shabas Logo'} height={45} />
                         </Link>
-                        {adminRole ?
-                            <>
-                                <Link href={'/clients'} className={'nav-item ' + (active == 'clients' ? 'active' : '')}>
-                                    <span className='label'>Clients</span>
-                                    <span className='count'>{totalClient ?? 0}</span>
-                                    <span className='label child-label'>/ Sites</span>
-                                    <span className='count'>{totalSite ?? 0}</span>
-                                </Link>
-                                <Link href={'/pocs'} className={'nav-item ' + (active == 'pocs' ? 'active' : '')}>
-                                    <span className='label'>POC</span>
-                                    <span className='count'>{totalPOC ?? 0}</span>
-                                </Link>
-                                <Link href={'/assessments'} className={'nav-item ' + (active == 'assessments' ? 'active' : '')}>
-                                    <span className='label'>Engagements</span>
-                                    <span className='count'>{totalEngagement ?? 0}</span>
-                                    <span className='label child-label'>/ Assessments</span>
-                                    <span className='count'>{totalAssessment ?? 0}</span>
-                                </Link>
-                                <Link href={'/questions'} className={'nav-item ' + (active == 'questions' ? 'active' : '')}>
-                                    <span className='label'>Question</span>
-                                    <span className='count'>{totalQuestion ?? 0}</span>
-                                </Link>
-                                <Link href={'/completed-assessments'} className={'nav-item ' + (active == 'completed-assessments' ? 'active' : '')}>
-                                    <span className='label'>
-                                        Completed Assessments
-                                    </span>
-                                </Link>
-                            </> :
-                            <>
-                                <Link href={'/ongoing-assessments'} className={'nav-item ' + (active == 'ongoing-assessments' ? 'active' : '')}>
-                                    <span className='label'>
-                                        Ongoing Assessments
-                                    </span>
-                                </Link>
-                                <Link href={'/review-assessments'} className={'nav-item ' + (active == 'review-assessments' ? 'active' : '')}>
-                                    <span className='label'>
-                                        Review Assessments
-                                    </span>
-                                </Link>
-                                <Link href={'/oversight-assessments'} className={'nav-item ' + (active == 'oversight-assessments' ? 'active' : '')}>
-                                    <span className='label'>
-                                        Oversight Assessments
-                                    </span>
-                                </Link>
-                            </>
-                        }
-                        <Switch checked={adminRole} onChange={() => setAdminRole(!adminRole)} />
+                        {getRoleNavItems(session.user.role)}
                     </div>
-                    <IconButton onClick={handleClick}>
-                        <Settings />
-                    </IconButton>
-                    <Menu
-                        anchorEl={anchorEl}
-                        open={open}
-                        onClose={handleClose}
-                    >
-                        <MenuItem onClick={handleClose}>Profile</MenuItem>
-                        <MenuItem onClick={() => { router.push('/management'); handleClose(); }}>User Management</MenuItem>
-                        <MenuItem onClick={handleLogout}>Logout</MenuItem>
-                    </Menu>
+                    {userFeatures}
                 </div>
                 {children}
             </main>
